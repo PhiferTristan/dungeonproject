3fe30ef9139975cd7cb56cc09fe8c1f5d1f9fb01
PhiferTristan <phifer.tristan@outlook.com>
2023-12-15 11:37:31 -0600

    added viewset for Backgrounds with list and retrieve methods

--- dungeonapi/views/__init__.py
+++ dungeonapi/views/__init__.py
@@ -3,3 +3,4 @@ from .playerusers import PlayerUserViewSet
 from .dungeonmasterusers import DungeonMasterUserViewSet
 from .races import RaceViewSet
 from .alignments import AlignmentViewSet
+from .backgrounds import BackgroundViewSet
--- dungeonproject/urls.py
+++ dungeonproject/urls.py
@@ -1,13 +1,14 @@
 from django.contrib import admin
 from django.urls import include, path
 from rest_framework import routers
-from dungeonapi.views import UserViewSet, RaceViewSet, AlignmentViewSet
+from dungeonapi.views import UserViewSet, RaceViewSet, AlignmentViewSet, BackgroundViewSet
 from django.conf.urls.static import static
 from . import settings
 
 router = routers.DefaultRouter(trailing_slash=False)
 router.register(r'races', RaceViewSet, "race")
-router.register(r'alignments', AlignmentViewSet, "alignments")
+router.register(r'alignments', AlignmentViewSet, "alignment")
+router.register(r'backgrounds', BackgroundViewSet, "background")
 
 urlpatterns = [
     path('', include(router.urls)),
